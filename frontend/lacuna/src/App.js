8d28cbcbad4683f851eba2dfff95064babe70578
Ian Macleod <idm2114@columbia.edu>
2021-01-24 18:48:03 -0500

    initial setup for react complete

---
+++ frontend/lacuna/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to empty lacuna. Soon to be great! 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
