b020ec3afd4d6d348ff1502036b39dcc84b1060d
ian macleod <idm2114@columbia.edu>
2021-02-11 23:48:14 +0000

    updating files after initial migration to server

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// used for making things display as loading prior to render
+// import Skeleton from 'react-loading-skeleton';
+// App.enable('trust proxy');
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to empty lacuna. Soon to be great! 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
